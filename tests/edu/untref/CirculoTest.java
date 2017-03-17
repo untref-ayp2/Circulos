6145215eb9f0338b32187265f48e096a99e56577
Santiago Rojo <santiago.rojo@valtech.com>
2017-03-17 12:57:49 -0300

    Agrega test para cubrir casos positivos y negativos

--- tests/edu/untref/CirculoTest.java
+++ tests/edu/untref/CirculoTest.java
@@ -13,4 +13,28 @@ public class CirculoTest {
 		Assert.assertTrue(circulo.contieneA(unPunto));
 	}
 
+	@Test
+	public void unCirculoSabeCalcularSiUnPuntoNoEstaContenidoEnEl() {
+		Circulo circulo = new Circulo(new Punto(1.0, 1.0), 3.0);
+		Punto unPunto = new Punto(5.0, 4.0);
+
+		Assert.assertFalse(circulo.contieneA(unPunto));
+	}
+
+	@Test
+	public void unCirculoSabeCalcularSiIntersectaConOtroCirculo() {
+		Circulo unCirculo = new Circulo(new Punto(1.0, 1.0), 3.0);
+		Circulo otroCirculo = new Circulo(new Punto(5.0, 4.0), 4.0);
+
+		Assert.assertTrue(unCirculo.intersectaCon(otroCirculo));
+	}
+
+	@Test
+	public void unCirculoSabeCalcularSiNoIntersectaConOtroCirculo() {
+		Circulo unCirculo = new Circulo(new Punto(1.0, 1.0), 1.0);
+		Circulo otroCirculo = new Circulo(new Punto(5.0, 4.0), 2.0);
+
+		Assert.assertFalse(unCirculo.intersectaCon(otroCirculo));
+	}
+	
 }
